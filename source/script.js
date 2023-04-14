8600a5840d9095871ac093b99790e6a7362edd15
tayga2410 <moldazhanov.eldar@gmail.com>
2023-04-15 02:53:44 +0600

    адаптивные декоративные элементы

---
+++ source/script.js
@@ -0,0 +1,17 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+let logo = document.querySelector('.main-header__logo-image');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+    logo.style.visibility = 'hidden';
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    logo.style.visibility = 'visible';
+  }
+});
